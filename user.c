31a8635e7890061e887d4baa37fbb6abb3b01e46
thedraino <43102350+thedraino@users.noreply.github.com>
2018-10-31 17:47:10 -0500

    Update user.c

--- user.c
+++ user.c
@@ -1,1 +1,11 @@
+// File: user.c
+// Created by: Andrew Audrain 
 
+// User process which is generated and scheduled by OSS
+#include "oss.h"
+
+int main ( int argc, char *argv[] ) {
+    printf ( "Hello, from user.\n" );
+  
+    return 0;
+}
