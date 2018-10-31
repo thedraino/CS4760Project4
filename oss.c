957fbeea4b8e1ccf96b8ec0aa78e92f8208a41ac
thedraino <43102350+thedraino@users.noreply.github.com>
2018-10-31 17:45:11 -0500

    Update oss.c

--- oss.c
+++ oss.c
@@ -1,1 +1,12 @@
+// File: oss.c
+// Created by: Andrew Audrain
 
+// Master process to simulate the OSS scheduler
+
+#include "oss.h"
+
+int main ( int argc, int *argv[] ) {
+    printf ( "Hello, from oss.\n" );
+  
+    return 0;
+}
