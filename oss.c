1655892b37796702ee034d3f5c2b6712b9bf8036
Andrew Audrain <43102350+thedraino@users.noreply.github.com>
2018-11-27 14:44:59 -0600

    Update oss.c
    Added queue code.

--- oss.c
+++ oss.c
@@ -3,10 +3,101 @@
 
 // Master process to simulate the OSS scheduler
 
-#include "oss.h"
+// Note: Queue code is gotten from https://www.geeksforgeeks.org/queue-set-1introduction-and-array-implementation/
+
+#include "project4.h"
+
+/* Structures */
+// A structure to represent a queue
+typedef struct {
+	int front, rear, size;
+	unsigned capacity;
+	int *array;  
+	int pid;
+} Queue;
+                   
+/* Function Prototypes */
+Queue* createQueue ( unsigned capacity );
+int isFull ( Queue* queue ); 
+int isEmpty ( Queue* queue );
+void enqueue ( Queue* queue, int item );
+int dequeue ( Queue* queue );
+int front ( Queue* queue );
+int rear ( Queue* queue );
+
+/*************************************************************************************************************/
+/******************************************* Start of Main Function ******************************************/
+/*************************************************************************************************************/
 
 int main ( int argc, int *argv[] ) {
     printf ( "Hello, from oss.\n" );
   
     return 0;
+}
+
+/***************************************************************************************************************/
+/******************************************* End of Main Function **********************************************/
+/***************************************************************************************************************/
+
+/* Function Definitions */
+// Function to create a queue of given capacity.
+// It initializes size of queue as 0.
+Queue* createQueue ( unsigned capacity ) {
+	Queue* queue = (Queue*) malloc ( sizeof ( Queue ) );
+	queue->capacity = capacity;
+	queue->front = queue->size = 0;
+	queue->rear = capacity - 1;	// This is important, see the enqueue
+	queue->array = (int*) malloc ( queue->capacity * sizeof ( int ) );
+
+	return queue;
+}
+
+// Queue is full when size becomes equal to the capacity
+int isFull ( Queue* queue ) {
+	return ( queue->size == queue->capacity );
+}
+
+// Queue is empty when size is 0
+int isEmpty ( Queue* queue ) { 
+	return ( queue->size == 0 );
+}
+
+// Function to add an item to the queue.
+// It changes rear and size.
+void enqueue ( Queue* queue, int item ) {
+	if ( isFull ( queue ) ) 
+		return;
+	
+	queue->rear = ( queue->rear + 1 ) % queue->capacity;
+	queue->array[queue->rear] = item;
+	queue->size = queue->size + 1;
+}
+
+// Function to remove an item from queue.
+// It changes front and size.
+int dequeue ( Queue* queue ) {
+	if ( isEmpty ( queue ) )
+		return INT_MIN;
+
+	int item = queue->array[queue->front];
+	queue->front = ( queue->front + 1 ) % queue->capacity;
+	queue->size = queue->size - 1;
+
+	return item;
+}
+
+// Function to get front of queue.
+int front ( Queue* queue ) {
+	if ( isEmpty ( queue ) )
+		return INT_MIN;
+
+	return queue->array[queue->front];
+}
+
+// Function to get rear of queue.
+int rear ( Queue* queue ) {
+	if ( isEmpty ( queue ) )
+		return INT_MIN;
+
+	return queue->array[queue->rear];
 }
